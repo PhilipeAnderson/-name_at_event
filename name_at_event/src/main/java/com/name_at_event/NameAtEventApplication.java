0aa10548493eb3cbde120534b64d29466cf0296f
PhilipeAnderson <philipeacampos@hotmail.com>
2021-01-12 16:13:53 -0300

    Start with SpringBoot

---
+++ name_at_event/src/main/java/com/name_at_event/NameAtEventApplication.java
@@ -0,0 +1,13 @@
+package com.name_at_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameAtEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameAtEventApplication.class, args);
+	}
+
+}
---
+++ name_at_event/src/test/java/com/name_at_event/NameAtEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.name_at_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameAtEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
